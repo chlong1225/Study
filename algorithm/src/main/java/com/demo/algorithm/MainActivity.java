30280ebdbd17747697a03c2cb42f9f46b071591c
long25 <chenglongstrong@163.com>
2021-05-01 17:54:49 +0800

    log_调整Lru算法并补充测试代码.

--- algorithm/src/main/java/com/demo/algorithm/MainActivity.java
+++ algorithm/src/main/java/com/demo/algorithm/MainActivity.java
@@ -1,6 +1,9 @@
 package com.demo.algorithm;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.demo.algorithm.algo.LruLinkedList;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        testLru();
+    }
+
+    private void testLru() {
+        LruLinkedList<String> list = new LruLinkedList<>();
+        list.printAll();
+        Log.e("LruLinkedList", "testLru: 开始添加数据");
+        for (int i = 0; i < 10; i++) {
+            list.add("" + i);
+        }
+        list.printAll();
+        Log.e("LruLinkedList", "testLru: 添加重复数据5");
+        list.add("5");
+        list.printAll();
+        Log.e("LruLinkedList", "testLru: 超出长度");
+        list.add("10");
+        list.printAll();
+        Log.e("LruLinkedList", "testLru: 添加重复数据10");
+        list.add("10");
+        list.printAll();
     }
 }
--- algorithm/src/main/java/com/demo/algorithm/algo/LruLinkedList.java
+++ algorithm/src/main/java/com/demo/algorithm/algo/LruLinkedList.java
@@ -12,7 +12,7 @@ public class LruLinkedList<T> {
     private static final String TAG = "LruLinkedList";
 
     //最大容量为20
-    private static final int DEFAULT_CAPACITY = 20;
+    private static final int DEFAULT_CAPACITY = 10;
 
     //链表容量
     private int capacity = DEFAULT_CAPACITY;
@@ -32,47 +32,48 @@ public class LruLinkedList<T> {
             capacity = DEFAULT_CAPACITY;
         }
         this.capacity = capacity;
-        head = new SNode();
+        head = null;
         length = 0;
     }
 
     public void add(T data) {
         SNode<T> sNode = new SNode<>(data, null);
-        SNode p = head;
-        if (p == null) {
-            head = sNode;
-            length = 1;
-            return;
-        }
-        if (p.next == null) {
-            if (p.data == data) {
-                length = 1;
-                return;
-            }
-            head = sNode;
-            head.next = p;
-            length = 2;
-        }
-        SNode pre = head;
+        SNode<T> p = head;
+        //1,删除相同的节点数据
+        SNode<T> pre = head;
         while (p != null) {
-            if (p.data == data) {
-                //删除当前数据
+            //默认只支持String类型的数据
+            if (p.data.equals(data)) {
+                if (pre == p) {
+                    //链表头重复了不执行操作
+                    return;
+                }
                 pre.next = p.next;
-                SNode tem = head;
-                head = sNode;
-                head.next = tem;
-                return;
+                length--;
+                break;
             } else {
                 pre = p;
                 p = p.next;
             }
         }
-        if (length == capacity) {
-            pre.next = null;
+
+        //2,判断是否超出长度,删除尾结点
+        if (length >= capacity) {
+            SNode<T> p1 = null;
+            SNode<T> q1 = head;
+            while (q1.next != null) {
+                p1 = q1;
+                q1 = q1.next;
+            }
+            p1.next = null;
+            length--;
         }
-        SNode tem = head;
+
+        //3,添加节点
+        SNode<T> tem = head;
         head = sNode;
         head.next = tem;
+        length++;
     }
 
     public void printAll() {
@@ -80,7 +81,7 @@ public class LruLinkedList<T> {
             Log.e(TAG, "printAll: 链表为空");
             return;
         }
-        SNode p = head;
+        SNode<T> p = head;
         while (p != null) {
             Log.e(TAG, "printAll: data = " + p.data.toString());
             p = p.next;
@@ -91,12 +92,12 @@ public class LruLinkedList<T> {
     public static class SNode<T>{
 
         private T data;
-        private SNode next;
+        private SNode<T> next;
 
         public SNode() {
         }
 
-        public SNode(T data, SNode next) {
+        public SNode(T data, SNode<T> next) {
             this.data = data;
             this.next = next;
         }
