7a834ea3a2d0ea8d071a27aadd0fff07c63d64d9
chenglong <android1225>
2021-06-16 17:57:25 +0800

    log_添加集合测试类。

--- app/src/main/java/com/demo/study/MainActivity.kt
+++ app/src/main/java/com/demo/study/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.demo.study
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import com.chl.common.utils.LogUtil
 import com.demo.study.databinding.ActivityMainBinding
@@ -8,20 +9,23 @@ import com.demo.study.databinding.ActivityMainBinding
 class MainActivity : BaseActivity<ActivityMainBinding>() {
 
     lateinit var mKotlin: Button
+    lateinit var mSet: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mKotlin = findViewById(R.id.btn_kotlin);
-        /*mKotlin.setOnClickListener(object : View.OnClickListener {
+        mSet = findViewById(R.id.btn_set)
+        mKotlin.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
+                LogUtil.e("AAAA","onClick : kotlin")
                 KotlinActivity.openSelf(this@MainActivity)
             }
-        })*/
-        mKotlin.setOnClickListener {
-            LogUtil.e("AAAA","onClick")
-            KotlinActivity.openSelf(this@MainActivity)
+        })
+        mSet.setOnClickListener {
+            LogUtil.e("AAAA","onClick : set")
+            SetActivity.openSelf(this@MainActivity)
         }
-        /*mKotlin.setOnClickListener({ v: View -> KotlinActivity.openSelf(this@MainActivity) })*/
+//        mKotlin.setOnClickListener({ v: View -> KotlinActivity.openSelf(this@MainActivity) })
     }
 }
---
+++ app/src/main/java/com/demo/study/SetActivity.kt
@@ -0,0 +1,25 @@
+package com.demo.study
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import com.demo.study.databinding.ActivitySetBinding
+
+/**
+ * create by chenglong on 6/16/21
+ * description :
+ */
+class SetActivity : BaseActivity<ActivitySetBinding>() {
+
+    companion object {
+
+        fun openSelf(context: Context) {
+            context.startActivity(Intent(context, SetActivity::class.java))
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_set)
+    }
+}
