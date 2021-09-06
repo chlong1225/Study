0f8c12696944835d82ddc1ad2556b74e9d03cd11
chenglong <android1225>
2021-09-06 09:33:31 +0800

    log_添加删除链表中重复元素的代码。

---
+++ algorithm/src/main/java/com/demo/algorithm/leetcode/DeleteRepeatLink.java
@@ -0,0 +1,60 @@
+package com.demo.algorithm.leetcode;
+
+/**
+ * create by chenglong on 9/6/21
+ * description : 删除排序链表中的重复元素
+ *
+ *  存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除所有重复的元素，使每个元素 只出现一次 。
+ *  返回同样按升序排列的结果链表。
+ *
+ *  示例1
+ *  输入：head = [1,1,2]
+ *  输出：[1,2]
+ *
+ *  示例2
+ *  输入：head = [1,1,2,3,3]
+ *  输出：[1,2,3]
+ *
+ *
+ */
+public class DeleteRepeatLink {
+
+    public static ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode result = new ListNode();
+        ListNode start = result;
+        start.next = head;
+        start = start.next;
+        while (head != null) {
+            if (head.val != start.val) {
+                start.next = head;
+                start = start.next;
+            }
+            head = head.next;
+        }
+        if (start.next != null) {
+            start.next = null;
+        }
+        return result.next;
+    }
+
+    public static class ListNode {
+
+        int val;
+        public ListNode next;
+
+        public ListNode() {
+        }
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
