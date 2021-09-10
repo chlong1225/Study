8279e95181c3e58af9abdbfe88c3246f3302571f
chenglong <android1225>
2021-09-10 14:10:57 +0800

    log_添加判断环形链表的代码。

---
+++ algorithm/src/main/java/com/demo/algorithm/leetcode/ListNode.java
@@ -0,0 +1,17 @@
+package com.demo.algorithm.leetcode;
+
+/**
+ * create by chenglong on 9/10/21
+ * description :
+ */
+public class ListNode {
+
+    public int val;
+    public ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+
+}
