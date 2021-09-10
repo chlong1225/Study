e04f60eff5892fd660e3f67b86aaaa517da27313
chenglong <android1225>
2021-09-10 14:43:50 +0800

    log_添加二叉树后序遍历的代码。

---
+++ algorithm/src/main/java/com/demo/algorithm/leetcode/tree/PostOrderTraverse.java
@@ -0,0 +1,28 @@
+package com.demo.algorithm.leetcode.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * create by chenglong on 9/10/21
+ * description : 二叉树的后序遍历
+ *
+ *  给定一个二叉树，返回它的 后序 遍历。
+ */
+public class PostOrderTraverse {
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        getResult(root, result);
+        return result;
+    }
+
+    private void getResult(TreeNode root, List<Integer> result) {
+        if (root != null) {
+            getResult(root.left, result);
+            getResult(root.right, result);
+            result.add(root.val);
+        }
+    }
+
+}
