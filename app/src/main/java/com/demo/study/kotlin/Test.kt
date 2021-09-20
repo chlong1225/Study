b1046d356fb76d8b88f192dbf053c273c06d33c0
long25 <chenglongstrong@163.com>
2021-09-20 19:04:16 +0800

    log_添加时间转换的工具类.

--- app/src/main/java/com/demo/study/kotlin/Test.kt
+++ app/src/main/java/com/demo/study/kotlin/Test.kt
@@ -2,13 +2,6 @@ package com.demo.study.kotlin
 
 import com.chl.common.utils.LogUtil
 
-fun Animal.sleep(){
-    LogUtil.e("AAAA", "animal : sleep")
-}
-
-fun Cat.sleep(){
-    LogUtil.e("AAAA", "cat : sleep")
-}
 
 /**
  * Created by chl on 2021/9/14.
@@ -17,42 +10,11 @@ fun Cat.sleep(){
 class Test {
 
     fun call() {
-        val animal: Animal = Cat()
-        animal.eat()
-        animal.sleep()
-        Outer().call()
 
-        sleep(Animal())
-        sleep(Cat())
-    }
-
-    fun sleep(animal: Animal) {
-        animal.sleep()
-    }
-}
-
-class Inner{
-
-    fun print(){
-        LogUtil.e("AAAA", "print inner");
-    }
-}
-
-class Outer{
-
-    fun Inner.printName(){
-        print()
-        this@Outer.print()
-    }
-
-    fun print(){
-        LogUtil.e("AAAA", "print outer");
-    }
-
-    fun call() {
-        Inner().printName()
     }
 
 }
+
+
 
 
