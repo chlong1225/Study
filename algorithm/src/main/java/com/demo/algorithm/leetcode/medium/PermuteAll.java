cc0005bc1b8ef0ffbc9cd91e6993fd17ea5a3b49
long25 <chenglongstrong@163.com>
2021-10-25 20:20:17 +0800

    log_添加获取全排列的代码.

---
+++ algorithm/src/main/java/com/demo/algorithm/leetcode/medium/PermuteAll.java
@@ -0,0 +1,60 @@
+package com.demo.algorithm.leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by chl on 2021/10/25.
+ * description : 全排列
+ *
+ * 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
+ *
+ * 示例 1：
+ * 输入：nums = [1,2,3]
+ * 输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ *
+ * 示例 2：
+ * 输入：nums = [0,1]
+ * 输出：[[0,1],[1,0]]
+ *
+ * 示例 3：
+ * 输入：nums = [1]
+ * 输出：[[1]]
+ *  
+ * 提示：
+ * 1 <= nums.length <= 6
+ * -10 <= nums[i] <= 10
+ * nums 中的所有整数 互不相同
+ */
+public class PermuteAll {
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        int length = nums.length;
+        List<Integer> data = new ArrayList<>();
+        data.add(nums[0]);
+        result.add(data);
+        if (length == 1) {
+            return result;
+        }
+        List<List<Integer>> next = new ArrayList<>();
+        for (int i = 1; i < length; i++) {
+            int tem = nums[i];
+            int size = result.size();
+            for (int j = 0; j < size; j++) {
+                List<Integer> items = result.get(j);
+                int itemSize = items.size();
+                for (int k = 0; k < itemSize + 1; k++) {
+                    List<Integer> item = new ArrayList<>();
+                    item.addAll(items);
+                    item.add(k, tem);
+                    next.add(item);
+                }
+            }
+            result.clear();
+            result.addAll(next);
+            next.clear();
+        }
+        return result;
+    }
+}
