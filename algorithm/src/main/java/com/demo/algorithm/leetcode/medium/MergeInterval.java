ec0242c5d67594537be8cc73f1864d5336dbbf24
long25 <chenglongstrong@163.com>
2021-10-31 10:44:53 +0800

    log_优化区间合并的代码.

--- algorithm/src/main/java/com/demo/algorithm/leetcode/medium/MergeInterval.java
+++ algorithm/src/main/java/com/demo/algorithm/leetcode/medium/MergeInterval.java
@@ -25,37 +25,64 @@ package com.demo.algorithm.leetcode.medium;
 public class MergeInterval {
 
     public static int[][] merge(int[][] intervals) {
-        //1，将区间进行排序,使用冒泡原地排序
+        //1，合并加排序
         int length = intervals.length;
         if (length == 1) {
             return intervals;
         }
+        int count = 0;
         for (int i = 0; i < length - 1; i++) {
+            if (intervals[i] == null) {
+                continue;
+            }
             for (int j = i + 1; j < length; j++) {
-                if (intervals[i][0] > intervals[j][0]) {
-                    int[] tem = intervals[i];
-                    intervals[i] = intervals[j];
-                    intervals[j] = tem;
+                if (intervals[j] == null) {
+                    continue;
+                }
+                if (intervals[i][0] <= intervals[j][0]) {
+                    if (intervals[j][0] <= intervals[i][1]) {
+                        //合并
+                        intervals[i][1] = Math.max(intervals[i][1], intervals[j][1]);
+                        intervals[j] = null;
+                        count++;
+                    }
+                } else {
+                    if (intervals[i][0] <= intervals[j][1]) {
+                        //合并
+                        intervals[i][0] = intervals[j][0];
+                        intervals[i][1] = Math.max(intervals[i][1], intervals[j][1]);
+                        intervals[j] = null;
+                        count++;
+                    } else {
+                        //交换
+                        int[] tem = intervals[j];
+                        intervals[j] = intervals[i];
+                        intervals[i] = tem;
+                    }
                 }
             }
         }
-        //2，合并覆盖相同区间
-        int startIndex = 0;
-        int[] result = intervals[0];
+        //2,再次检查合并.之前存在遍历时无法合并,当前位置数据交换后可以合并,但当前数据之前已经比较过
+        int[] pre = intervals[0];
         for (int i = 1; i < length; i++) {
-            if (intervals[i][0] <= result[1]) {
-                //需要进行合并
-                result[1] = Math.max(result[1], intervals[i][1]);
-            } else {
-                startIndex++;
-                result = intervals[i];
-                intervals[startIndex] = result;
+            if (intervals[i] != null) {
+                if (pre[1] >= intervals[i][0]) {
+                    pre[1] = Math.max(pre[1], intervals[i][1]);
+                    intervals[i] = null;
+                    count++;
+                } else {
+                    pre = intervals[i];
+                }
             }
         }
-        int[][] data = new int[startIndex + 1][intervals[0].length];
-        for (int i = 0; i <= startIndex; i++) {
-            data[i] = intervals[i];
+        int[][] result = new int[length - count][2];
+        int index = 0;
+        for (int i = 0; i < length; i++) {
+            if (intervals[i] != null) {
+                result[index] = intervals[i];
+                index++;
+            }
         }
-        return data;
+        return result;
     }
 }
