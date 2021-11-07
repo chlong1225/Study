a98da27f3483df93dc93f345ea6b4183f4c28c9a
long25 <chenglongstrong@163.com>
2021-11-07 19:16:01 +0800

    feat_添加解码方法的代码.

---
+++ algorithm/src/main/java/com/demo/algorithm/leetcode/medium/DecodeStr.java
@@ -0,0 +1,107 @@
+package com.demo.algorithm.leetcode.medium;
+
+/**
+ * Created by chl on 2021/11/7.
+ * description : 解码方法
+ *
+ * 一条包含字母 A-Z 的消息通过以下映射进行了 编码 ：
+ * 'A' -> 1
+ * 'B' -> 2
+ * ...
+ * 'Z' -> 26
+ * 要 解码 已编码的消息，所有数字必须基于上述映射的方法，反向映射回字母（可能有多种方法）。例如，"11106" 可以映射为：
+ * "AAJF" ，将消息分组为 (1 1 10 6)
+ * "KJF" ，将消息分组为 (11 10 6)
+ * 注意，消息不能分组为  (1 11 06) ，因为 "06" 不能映射为 "F" ，这是由于 "6" 和 "06" 在映射中并不等价。
+ * 给你一个只含数字的 非空 字符串 s ，请计算并返回 解码 方法的 总数 。
+ * 题目数据保证答案肯定是一个 32 位 的整数。
+ *
+ * 示例 1：
+ * 输入：s = "12"
+ * 输出：2
+ * 解释：它可以解码为 "AB"（1 2）或者 "L"（12）。
+ *
+ * 示例 2：
+ * 输入：s = "226"
+ * 输出：3
+ * 解释：它可以解码为 "BZ" (2 26), "VF" (22 6), 或者 "BBF" (2 2 6) 。
+ *
+ * 示例 3：
+ * 输入：s = "0"
+ * 输出：0
+ * 解释：没有字符映射到以 0 开头的数字。
+ * 含有 0 的有效映射是 'J' -> "10" 和 'T'-> "20" 。
+ * 由于没有字符，因此没有有效的方法对此进行解码，因为所有数字都需要映射。
+ *
+ * 示例 4：
+ * 输入：s = "06"
+ * 输出：0
+ * 解释："06" 不能映射到 "F" ，因为字符串含有前导 0（"6" 和 "06" 在映射中并不等价）。
+ *  
+ * 提示：
+ * 1 <= s.length <= 100
+ * s 只包含数字，并且可能包含前导零。
+ */
+public class DecodeStr {
+
+    public static int numDecodings(String s) {
+        if (s.charAt(0) == '0') {
+            return 0;
+        }
+        int length = s.length();
+        int a1 = 1;
+        if (length == 1) {
+            return a1;
+        }
+        int a2 = 1;
+        int index0 = s.charAt(0) - '0';
+        int index1 = s.charAt(1) - '0';
+        if (index0 * 10 + index1 <= 26) {
+            if (index1 == 0) {
+                a2 = 1;
+            } else {
+                a2 = 2;
+            }
+        } else {
+            if (index1 == 0) {
+                a2 = 0;
+            } else {
+                a2 = 1;
+            }
+        }
+        if (length == 2) {
+            return a2;
+        }
+        int[] result = new int[length + 1];
+        result[0] = 1;
+        result[1] = 1;
+        result[2] = a2;
+        for (int i = 2; i < length; i++) {
+            int number = s.charAt(i) - '0';
+            int pre = s.charAt(i - 1) - '0';
+            if (number == 0) {
+                if (pre == 0) {
+                    return 0;
+                } else {
+                    if (pre * 10 + number <= 26) {
+                        result[i + 1] = result[i - 1];
+                    } else {
+                        return 0;
+                    }
+                }
+            } else {
+                if (pre == 0) {
+                    result[i + 1] = result[i];
+                } else {
+                    if (pre * 10 + number <= 26) {
+                        result[i + 1] = result[i] + result[i - 1];
+                    } else {
+                        result[i + 1] = result[i];
+                    }
+                }
+            }
+        }
+        return result[length];
+    }
+
+}
