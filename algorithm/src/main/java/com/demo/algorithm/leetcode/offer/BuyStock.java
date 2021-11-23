e3deb8b8ac51f0cb3de8336fdf5061a8b73efe8a
long25 <chenglongstrong@163.com>
2021-11-23 21:27:10 +0800

    feat_添加使用双指针的方式获取股票最大利润的代码.

--- algorithm/src/main/java/com/demo/algorithm/leetcode/offer/BuyStock.java
+++ algorithm/src/main/java/com/demo/algorithm/leetcode/offer/BuyStock.java
@@ -46,4 +46,63 @@ public class BuyStock {
         }
         return sell;
     }
+
+    //使用类似单调栈的方式
+    public int maxProfit2(int[] prices) {
+        int length = prices.length;
+        if (length <= 1) {
+            return 0;
+        }
+        //用于存放当前位置向右的最大值
+        int[] maxRight = new int[length];
+        maxRight[length - 1] = prices[length - 1];
+        int max = prices[length - 1];
+        for (int i = length - 1; i >= 0; i--) {
+            if (max < prices[i]) {
+                max = prices[i];
+                maxRight[i] = prices[i];
+            } else {
+                maxRight[i] = max;
+            }
+        }
+        int profit = 0;
+        for (int i = 0; i < length; i++) {
+            if (maxRight[i] - prices[i] > profit) {
+                profit = maxRight[i] - prices[i];
+            }
+        }
+        return profit;
+    }
+
+    //使用双指针的方式
+    public int maxProfit3(int[] prices) {
+        int length = prices.length;
+        if (length <= 1) {
+            return 0;
+        }
+        int min = prices[0];
+        int max = prices[1];
+        int profit = 0;
+        for (int i = 1; i < length; i++) {
+            int price = prices[i];
+            if (price < min) {
+                //计算上一个区间的最大差价
+                if (max - min > profit) {
+                    profit = max - min;
+                }
+                min = price;
+                if (i + 1 < length) {
+                    max = prices[i + 1];
+                } else {
+                    max = min;
+                }
+            } else if (price > max) {
+                max = price;
+            }
+        }
+        if (max - min > profit) {
+            profit = max - min;
+        }
+        return profit;
+    }
 }
