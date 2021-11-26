296684bfd717d089d9e7b74a4220857f210996f7
chenglong <android1225>
2021-11-26 12:34:36 +0800

    feat_使用广度优先bfs搜索二叉树中指定大小的节点。

---
+++ algorithm/src/main/java/com/demo/algorithm/leetcode/easy/tree/SearchNode.java
@@ -0,0 +1,61 @@
+package com.demo.algorithm.leetcode.easy.tree;
+
+import com.demo.algorithm.leetcode.entity.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * create on 11/26/21
+ * @author chenglong
+ * description : 二叉搜索树中的搜索
+ *
+ * 给定二叉搜索树（BST）的根节点和一个值。 你需要在BST中找到节点值等于给定值的节点。 返回以该节点为根的子树。 如果节点不存在，则返回 NULL。
+ * 例如，
+ * 给定二叉搜索树:
+ *
+ *         4
+ *        / \
+ *       2   7
+ *      / \
+ *     1   3
+ *
+ * 和值: 2
+ * 你应该返回如下子树:
+ *
+ *       2
+ *      / \
+ *     1   3
+ * 在上述示例中，如果要找的值是 5，但因为没有节点值为 5，我们应该返回 NULL。
+ */
+public class SearchNode {
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        //使用广度优先bfs
+        List<TreeNode> datas = new ArrayList<>();
+        List<TreeNode> next = new ArrayList<>();
+        datas.add(root);
+        while (!datas.isEmpty()) {
+            int size = datas.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode tem = datas.get(i);
+                if (tem.val == val) {
+                    return tem;
+                }
+                if (tem.left != null) {
+                    next.add(tem.left);
+                }
+                if (tem.right != null) {
+                    next.add(tem.right);
+                }
+            }
+            datas.clear();
+            datas.addAll(next);
+            next.clear();
+        }
+        return null;
+    }
+}
