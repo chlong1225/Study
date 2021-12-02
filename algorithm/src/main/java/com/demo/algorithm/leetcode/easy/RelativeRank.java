07f93534cf3a97cab2e67455231b46c3e3bf449d
chenglong <android1225>
2021-12-02 09:16:40 +0800

    feat_添加相对名次的代码。

---
+++ algorithm/src/main/java/com/demo/algorithm/leetcode/easy/RelativeRank.java
@@ -0,0 +1,73 @@
+package com.demo.algorithm.leetcode.easy;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.function.ToDoubleFunction;
+import java.util.function.ToIntFunction;
+import java.util.function.ToLongFunction;
+
+/**
+ * create on 12/2/21
+ *
+ * @author chenglong
+ * description : 相对名次
+ *
+ * 给你一个长度为 n 的整数数组 score ，其中 score[i] 是第 i 位运动员在比赛中的得分。所有得分都 互不相同 。
+ * 运动员将根据得分 决定名次 ，其中名次第 1 的运动员得分最高，名次第 2 的运动员得分第 2 高，依此类推。运动员的名次决定了他们的获奖情况：
+ * 名次第 1 的运动员获金牌 "Gold Medal" 。
+ * 名次第 2 的运动员获银牌 "Silver Medal" 。
+ * 名次第 3 的运动员获铜牌 "Bronze Medal" 。
+ * 从名次第 4 到第 n 的运动员，只能获得他们的名次编号（即，名次第 x 的运动员获得编号 "x"）。
+ * 使用长度为 n 的数组 answer 返回获奖，其中 answer[i] 是第 i 位运动员的获奖情况。
+ *
+ * 示例 1：
+ * 输入：score = [5,4,3,2,1]
+ * 输出：["Gold Medal","Silver Medal","Bronze Medal","4","5"]
+ * 解释：名次为 [1st, 2nd, 3rd, 4th, 5th] 。
+ *
+ * 示例 2：
+ * 输入：score = [10,3,8,9,4]
+ * 输出：["Gold Medal","5","Bronze Medal","Silver Medal","4"]
+ * 解释：名次为 [1st, 5th, 3rd, 2nd, 4th] 。
+ *  
+ * 提示：
+ * n == score.length
+ * 1 <= n <= 104
+ * 0 <= score[i] <= 106
+ * score 中的所有值 互不相同
+ */
+public class RelativeRank {
+
+    public String[] findRelativeRanks(int[] score) {
+        int length = score.length;
+        String[] result = new String[length];
+        //1，用于保持分数与位置的对照关系，分数排序后位置会变化
+        Map<Integer, Integer> datas = new HashMap<>();
+        for (int i = 0; i < length; i++) {
+            datas.put(score[i], i);
+        }
+        //2，默认排序后分数从小到大
+        Arrays.sort(score);
+        //3，遍历分数并赋值获奖情况
+        int rank = 1;
+        for (int i = length - 1; i >= 0; i--) {
+            //当前分数的位置
+            int index = datas.get(score[i]);
+            if (rank == 1) {
+                result[index] = "Gold Medal";
+            } else if (rank == 2) {
+                result[index] = "Silver Medal";
+            } else if (rank == 3) {
+                result[index] = "Bronze Medal";
+            } else {
+                result[index] = "" + rank;
+            }
+            rank++;
+        }
+        return result;
+    }
+
+}
