5b7e4d406dcec625c5ed2f446c2dffa3eceaf39a
long25 <chenglongstrong@163.com>
2022-01-01 17:32:50 +0800

    feat_优化一维数组转二维数组的代码。

--- algorithm/src/main/java/com/demo/algorithm/leetcode/easy/ChangeArray.java
+++ algorithm/src/main/java/com/demo/algorithm/leetcode/easy/ChangeArray.java
@@ -55,4 +55,18 @@ public class ChangeArray {
         }
         return result;
     }
+
+    public int[][] construct2DArray2(int[] original, int m, int n) {
+        int length = original.length;
+        if (m * n != length) {
+            return new int[0][0];
+        }
+        int[][] result = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                result[i][j] = original[i * n + j];
+            }
+        }
+        return result;
+    }
 }
