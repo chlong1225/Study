1a63bb9016923ee24f8c49311fc005eabc6c89bd
chenglong <android1225>
2022-01-04 14:40:21 +0800

    feat_升级as版本后同步修改项目配置，升级第三方库版本。

--- common/src/main/java/com/chl/common/utils/TimeUtil.kt
+++ common/src/main/java/com/chl/common/utils/TimeUtil.kt
@@ -2,6 +2,7 @@ package com.chl.common.utils
 
 import java.text.ParsePosition
 import java.text.SimpleDateFormat
+import java.util.*
 
 /**
  * Created by chl on 2021/9/20.
@@ -13,13 +14,13 @@ object TimeUtil {
      * 时间戳转换为格式化的时间
      */
     fun longToStr(time: Long, format: String): String {
-        val simpleDateFormat = SimpleDateFormat(format)
+        val simpleDateFormat = SimpleDateFormat(format, Locale.ENGLISH)
         return simpleDateFormat.format(time)
     }
 
     fun strToLong(time: String, format: String): Long {
-        val simpleDateFormat = SimpleDateFormat(format)
+        val simpleDateFormat = SimpleDateFormat(format, Locale.ENGLISH)
         val parse = simpleDateFormat.parse(time, ParsePosition((0)))
-        return parse.time
+        return parse!!.time
     }
 }
