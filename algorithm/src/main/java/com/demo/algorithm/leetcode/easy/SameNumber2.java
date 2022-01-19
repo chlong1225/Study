38ab8148a294cdde9c9c4af05d9867cdfa8698d9
chenglong <android1225>
2022-01-19 08:55:39 +0800

    feat_添加存在重复元素II的代码。

--- algorithm/src/main/java/com/demo/algorithm/leetcode/easy/SameNumber2.java
+++ algorithm/src/main/java/com/demo/algorithm/leetcode/easy/SameNumber2.java
@@ -1,22 +1,23 @@
 package com.demo.algorithm.leetcode.easy;
 
-import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * create by chenglong on 9/16/21
  * description : 存在重复元素 II
  *
- * 给定一个整数数组和一个整数 k，判断数组中是否存在两个不同的索引 i 和 j，使得 nums [i] = nums [j]，并且 i 和 j 的差的 绝对值 至多为 k。
+ * 给定一个整数数组和一个整数k，判断数组中是否存在两个不同的索引i和j，使得nums[i] = nums[j]，并且i和j的差的绝对值至多为 k。
  *
- * 示例 1:
+ * 示例1:
  * 输入: nums = [1,2,3,1], k = 3
  * 输出: true
  *
- * 示例 2:
+ * 示例2:
  * 输入: nums = [1,0,1,1], k = 1
  * 输出: true
  *
- * 示例 3:
+ * 示例3:
  * 输入: nums = [1,2,3,1,2,3], k = 2
  * 输出: false
  */
@@ -43,6 +44,26 @@ public class SameNumber2 {
             if (datas[i] == num) {
                 return true;
             }
+        }
+        return false;
+    }
+
+    public static boolean containsNearbyDuplicate2(int[] nums, int k) {
+        int length = nums.length;
+        if (length == 1) {
+            return false;
+        }
+        if (k == 0) {
+            return false;
+        }
+        //记录当前位置的值+位置index
+        Map<Integer, Integer> marks = new HashMap<>();
+        marks.put(nums[0], 0);
+        for (int i = 1; i < length; i++) {
+            if (marks.containsKey(nums[i]) && (i - marks.get(nums[i]) <= k)) {
+                return true;
+            }
+            marks.put(nums[i], i);
         }
         return false;
     }
