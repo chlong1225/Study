0408b7979ec44ad3af2fa79f1d7a7edbac781b6a
chenglong <android1225>
2022-02-23 10:57:49 +0800

    feat_使用WindowCompat实现沉浸式状态栏效果。

--- app/src/main/java/com/demo/study/StatusBarActivity.kt
+++ app/src/main/java/com/demo/study/StatusBarActivity.kt
@@ -6,9 +6,7 @@ import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.widget.FrameLayout
-import androidx.core.view.OnApplyWindowInsetsListener
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.*
 import com.demo.study.databinding.ActivityStatusBarBinding
 
 /**
@@ -16,6 +14,7 @@ import com.demo.study.databinding.ActivityStatusBarBinding
  * @author chenglong
  * description : 验证沉浸式状态栏的实现效果
  * https://mp.weixin.qq.com/s/AiCNJAi9CgYDE1UuzCboGg
+ * https://blog.csdn.net/StjunF/article/details/121840122
  */
 class StatusBarActivity : BaseActivity<ActivityStatusBarBinding>() {
 
@@ -35,8 +34,16 @@ class StatusBarActivity : BaseActivity<ActivityStatusBarBinding>() {
         super.onCreate(savedInstanceState)
         window.statusBarColor = Color.TRANSPARENT
 
-        getViewBinding().root.systemUiVisibility =
+        /*getViewBinding().root.systemUiVisibility =
             View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        ViewCompat.setOnApplyWindowInsetsListener(getViewBinding().btnCheck
+        ) { v, insets ->
+            val params = v.layoutParams as FrameLayout.LayoutParams
+            params.topMargin = insets.systemWindowInsetTop
+            insets
+        }*/
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         ViewCompat.setOnApplyWindowInsetsListener(getViewBinding().btnCheck
         ) { v, insets ->
             val params = v.layoutParams as FrameLayout.LayoutParams
