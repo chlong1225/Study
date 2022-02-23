ce1a8e1161f8ec99f3983cbd6a873464f922f9f0
chenglong <android1225>
2022-02-23 10:36:16 +0800

    feat_根布局为FrameLayout时使用代码设置的方式实现沉浸式状态栏效果。

--- app/src/main/java/com/demo/study/StatusBarActivity.kt
+++ app/src/main/java/com/demo/study/StatusBarActivity.kt
@@ -4,12 +4,18 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.widget.FrameLayout
+import androidx.core.view.OnApplyWindowInsetsListener
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.demo.study.databinding.ActivityStatusBarBinding
 
 /**
  * create on 2022/2/22
  * @author chenglong
- * description :
+ * description : 验证沉浸式状态栏的实现效果
+ * https://mp.weixin.qq.com/s/AiCNJAi9CgYDE1UuzCboGg
  */
 class StatusBarActivity : BaseActivity<ActivityStatusBarBinding>() {
 
@@ -28,6 +34,15 @@ class StatusBarActivity : BaseActivity<ActivityStatusBarBinding>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.statusBarColor = Color.TRANSPARENT
+
+        getViewBinding().root.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        ViewCompat.setOnApplyWindowInsetsListener(getViewBinding().btnCheck
+        ) { v, insets ->
+            val params = v.layoutParams as FrameLayout.LayoutParams
+            params.topMargin = insets.systemWindowInsetTop
+            insets
+        }
     }
 
 }
