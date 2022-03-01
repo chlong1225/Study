8c950c01d3bb9ed2c493a51903052165182e37c0
chenglong <android1225>
2022-03-01 13:03:04 +0800

    feat_优化获取Z字形变换的代码。

--- algorithm/src/main/java/com/demo/algorithm/leetcode/medium/ZTransform.java
+++ algorithm/src/main/java/com/demo/algorithm/leetcode/medium/ZTransform.java
@@ -5,15 +5,15 @@ package com.demo.algorithm.leetcode.medium;
  * @author chenglong
  * description : Z字形变换
  *
- * 将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行 Z 字形排列。
- * 比如输入字符串为 "PAYPALISHIRING" 行数为 3 时，排列如下：
+ * 将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行Z 字形排列。
+ * 比如输入字符串为 "PAYPALISHIRING"行数为 3 时，排列如下：
  * P   A   H   N
  * A P L S I I G
  * Y   I   R
  * 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："PAHNAPLSIIGYIR"。
  * 请你实现这个将字符串进行指定行数变换的函数：
  * string convert(string s, int numRows);
- *  
+ *
  * 示例 1：
  * 输入：s = "PAYPALISHIRING", numRows = 3
  * 输出："PAHNAPLSIIGYIR"
@@ -30,7 +30,7 @@ package com.demo.algorithm.leetcode.medium;
  * 示例 3：
  * 输入：s = "A", numRows = 1
  * 输出："A"
- *  
+ *
  * 提示：
  * 1 <= s.length <= 1000
  * s 由英文字母（小写和大写）、',' 和 '.' 组成
@@ -73,6 +73,41 @@ public class ZTransform {
                     }
                     if (index2 < length) {
                         builder.append(s.charAt(index2));
+                    }
+                }
+            }
+        }
+        return builder.toString();
+    }
+
+    public String convert2(String s, int numRows) {
+        int length = s.length();
+        if (length == 1 || numRows == 1) {
+            return s;
+        }
+        StringBuilder builder = new StringBuilder();
+        //1，记录Z字形一个重复使用的字符数量
+        int count = numRows * 2 - 2;
+        //2，计算重复Z字形的次数
+        int time = length / count;
+        for (int i = 0; i < numRows; i++) {
+            //添加完整的time次
+            for (int j = 0; j < time; j++) {
+                int index = i + j * count;
+                builder.append(s.charAt(index));
+                if (i > 0 && i < numRows - 1) {
+                    int index2 = (j + 1) * count - i;
+                    builder.append(s.charAt(index2));
+                }
+            }
+            //添加最后不完整的一行
+            int lastIndex = i + time * count;
+            if (lastIndex < length) {
+                builder.append(s.charAt(lastIndex));
+                if (i > 0 && i < numRows - 1) {
+                    int lastIndex2 = (time + 1) * count - i;
+                    if (lastIndex2 < length) {
+                        builder.append(s.charAt(lastIndex2));
                     }
                 }
             }
