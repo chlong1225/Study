3497370cb7c1defb20d645a55e73cffb0abaac49
long25 <chenglongstrong@163.com>
2022-03-26 21:56:07 +0800

    feat_优化判断对称二叉树的代码。

--- algorithm/src/main/java/com/demo/algorithm/leetcode/offer/Symmetric.java
+++ algorithm/src/main/java/com/demo/algorithm/leetcode/offer/Symmetric.java
@@ -95,4 +95,24 @@ public class Symmetric {
         }
         return true;
     }
+
+    public boolean isSymmetric2(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return dfs(root.left, root.right);
+    }
+
+    private boolean dfs(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        return dfs(left.left, right.right) && dfs(left.right, right.left);
+    }
 }
