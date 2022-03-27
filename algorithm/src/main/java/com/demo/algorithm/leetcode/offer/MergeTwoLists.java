8daf1cdd11519f86dc952b7682c93ed36876d38c
long25 <chenglongstrong@163.com>
2022-03-27 15:43:25 +0800

    feat_添加两个有序链表合并的代码。

---
+++ algorithm/src/main/java/com/demo/algorithm/leetcode/offer/MergeTwoLists.java
@@ -0,0 +1,54 @@
+package com.demo.algorithm.leetcode.offer;
+
+import com.demo.algorithm.leetcode.entity.ListNode;
+
+/**
+ * Created by chl on 2022/3/27.
+ * description : 剑指Offer25. 合并两个排序的链表
+ *
+ * 输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。
+ *
+ * 示例1：
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ *
+ * 限制：
+ * 0 <= 链表长度 <= 1000
+ */
+public class MergeTwoLists {
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode head;
+        if (l1.val < l2.val) {
+            head = l1;
+            l1 = l1.next;
+        } else {
+            head = l2;
+            l2 = l2.next;
+        }
+        ListNode p = head;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                p.next = l1;
+                l1 = l1.next;
+                p = p.next;
+            } else {
+                p.next = l2;
+                l2 = l2.next;
+                p = p.next;
+            }
+        }
+        if (l1 == null) {
+            p.next = l2;
+        } else {
+            p.next = l1;
+        }
+        return head;
+    }
+}
