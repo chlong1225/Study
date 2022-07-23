bf0a51c9dc3f61ddfbc6cb2029e4fe1c67fe98e1
chenglong <android1225>
2022-07-23 09:21:23 +0800

    feat_添加数组形成数对的代码。

---
+++ algorithm/src/main/java/com/demo/algorithm/leetcode/contest/week302/NumberOfPairs.java
@@ -0,0 +1,53 @@
+package com.demo.algorithm.leetcode.contest.week302;
+
+/**
+ * create on 2022/7/23
+ * @author chenglong
+ * description : 数组能形成多少数对
+ *
+ * 给你一个下标从0开始的整数数组nums。在一步操作中，你可以执行以下步骤：
+ * 从nums选出两个相等的整数
+ * 从nums中移除这两个整数，形成一个数对
+ * 请你在nums上多次执行此操作直到无法继续执行。
+ * 返回一个下标从0开始、长度为2的整数数组answer作为答案，其中answer[0]是形成的数对数目，answer[1]是对nums尽可能执行上述操作后剩下的整数数目。
+ *
+ * 示例 1：
+ * 输入：nums = [1,3,2,1,3,2,2]
+ * 输出：[3,1]
+ * 解释：
+ * nums[0] 和 nums[3] 形成一个数对，并从 nums 中移除，nums = [3,2,3,2,2] 。
+ * nums[0] 和 nums[2] 形成一个数对，并从 nums 中移除，nums = [2,2,2] 。
+ * nums[0] 和 nums[1] 形成一个数对，并从 nums 中移除，nums = [2] 。
+ * 无法形成更多数对。总共形成 3 个数对，nums 中剩下 1 个数字。
+ *
+ * 示例 2：
+ * 输入：nums = [1,1]
+ * 输出：[1,0]
+ * 解释：nums[0] 和 nums[1] 形成一个数对，并从 nums 中移除，nums = [] 。
+ * 无法形成更多数对。总共形成 1 个数对，nums 中剩下 0 个数字。
+ *
+ * 示例 3：
+ * 输入：nums = [0]
+ * 输出：[0,1]
+ * 解释：无法形成数对，nums 中剩下 1 个数字。
+ *
+ * 提示：
+ * 1 <= nums.length <= 100
+ * 0 <= nums[i] <= 100
+ */
+public class NumberOfPairs {
+
+    public int[] numberOfPairs(int[] nums) {
+        int[] counts = new int[101];
+        int length = nums.length;
+        for (int i = 0; i < length; i++) {
+            counts[nums[i]]++;
+        }
+        int count = 0;
+        for (int i = 0; i < 101; i++) {
+            count += (counts[i] / 2);
+        }
+        int last = length - 2 * count;
+        return new int[]{count, last};
+    }
+}
