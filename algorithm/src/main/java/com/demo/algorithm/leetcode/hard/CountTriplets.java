0ad2797051c6d087b31452fa680c701477d8b9ca
chenglong <android1225>
2023-03-30 10:06:55 +0800

    feat_添加按位与为零的三元组代码。

---
+++ algorithm/src/main/java/com/demo/algorithm/leetcode/hard/CountTriplets.java
@@ -0,0 +1,61 @@
+package com.demo.algorithm.leetcode.hard;
+
+/**
+ * create on 2023/3/30
+ * @author chenglong
+ * description : 按位与为零的三元组
+ *
+ * 给你一个整数数组nums，返回其中按位与三元组的数目。
+ * 按位与三元组是由下标(i,j,k)组成的三元组，并满足下述全部条件：
+ * 0 <= i < nums.length
+ * 0 <= j < nums.length
+ * 0 <= k < nums.length
+ * nums[i] & nums[j] & nums[k] == 0 ，其中 & 表示按位与运算符。
+ *
+ * 示例 1：
+ * 输入：nums = [2,1,3]
+ * 输出：12
+ * 解释：可以选出如下 i, j, k 三元组：
+ * (i=0, j=0, k=1) : 2 & 2 & 1
+ * (i=0, j=1, k=0) : 2 & 1 & 2
+ * (i=0, j=1, k=1) : 2 & 1 & 1
+ * (i=0, j=1, k=2) : 2 & 1 & 3
+ * (i=0, j=2, k=1) : 2 & 3 & 1
+ * (i=1, j=0, k=0) : 1 & 2 & 2
+ * (i=1, j=0, k=1) : 1 & 2 & 1
+ * (i=1, j=0, k=2) : 1 & 2 & 3
+ * (i=1, j=1, k=0) : 1 & 1 & 2
+ * (i=1, j=2, k=0) : 1 & 3 & 2
+ * (i=2, j=0, k=1) : 3 & 2 & 1
+ * (i=2, j=1, k=0) : 3 & 1 & 2
+ *
+ * 示例 2：
+ * 输入：nums = [0,0,0]
+ * 输出：27
+ *
+ * 提示：
+ * 1 <= nums.length <= 1000
+ * 0 <= nums[i] < 2^16
+ */
+public class CountTriplets {
+
+    public int countTriplets(int[] nums) {
+        int[] marks = new int[1 << 16];
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int tem = nums[i] & nums[j];
+                marks[tem]++;
+            }
+        }
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < marks.length; j++) {
+                if (marks[j] > 0 && (nums[i] & j) == 0) {
+                    count += marks[j];
+                }
+            }
+        }
+        return count;
+    }
+}
