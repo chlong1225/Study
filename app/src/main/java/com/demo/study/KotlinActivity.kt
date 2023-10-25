6fb3b972b554627a8ea615df3d729cb534c65b05
chenglong <android1225>
2023-10-25 18:49:16 +0800

    feat_添加kotlin测试代码。

--- app/src/main/java/com/demo/study/KotlinActivity.kt
+++ app/src/main/java/com/demo/study/KotlinActivity.kt
@@ -5,6 +5,18 @@ import android.content.Intent
 import android.os.Bundle
 import com.chl.common.utils.LogUtil
 import com.demo.study.databinding.ActivityKotlinBinding
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import java.lang.NullPointerException
 
 /**
  * create by chenglong on 6/7/21
@@ -21,39 +33,96 @@ class KotlinActivity : BaseActivity<ActivityKotlinBinding>() {
         }
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        test()
-    }
-
-    private fun getLength(str: String?): Int {
-        return str?.length ?: 0
-    }
-
-    private fun test() {
-//        LogUtil.e(TAG, "位运算左移 = " + (10 shl 2))
-//        LogUtil.e(TAG, "位运算右移 = " + (10 shr 2))
-//        LogUtil.e(TAG, "位运算无符号右移 = " + (10 ushr 2))
-//        LogUtil.e(TAG, "位运算与 = " + (10 and 2))
-//        LogUtil.e(TAG, "位运算或 = " + (10 or 2))
-//        LogUtil.e(TAG, "位运算异或 = " + (10 xor  2))
-//        LogUtil.e(TAG, "位运算非 = " + (10.inv()))
-
-        val a: Int = 100
-        val box1: Int? = a
-        val box2: Int? = a
-        LogUtil.e(TAG, "result1 = ${box1 == box2}")
-        LogUtil.e(TAG, "result2 = ${box1 === box2}")
-        val b: Int = 1000
-        val box3: Int? = b
-        val box4: Int? = b
-        LogUtil.e(TAG, "result3 = ${box3 == box4}")
-        LogUtil.e(TAG, "result4 = ${box3 === box4}")
-    }
-
     override fun buildViewBinding(): ActivityKotlinBinding {
         return ActivityKotlinBinding.inflate(layoutInflater)
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        getViewBinding().btnStart.setOnClickListener {
+//            start()
+//            testCoroutineContext()
+//            testCoroutineStart()
+//            testCoroutineExceptionHandler()
+        }
+    }
+
+    private fun testCoroutineExceptionHandler() {
+        val exceptionHandler = CoroutineExceptionHandler{coroutineContext, throwable ->
+            LogUtil.e(TAG, "testCoroutineExceptionHandler : 33 : ${coroutineContext[CoroutineName]} ;; ${coroutineContext} ;; $throwable")
+        }
+        GlobalScope.launch(CoroutineName("异常处理") + exceptionHandler) {
+            val job = launch {
+                LogUtil.e(TAG, "testCoroutineExceptionHandler : 11 : thread = ${Thread.currentThread().name}")
+                throw NullPointerException("测试异常")
+            }
+            job.join()
+            LogUtil.e(TAG, "testCoroutineExceptionHandler : 22 : thread = ${Thread.currentThread().name}")
+        }
+    }
+
+    private fun testCoroutineStart() {
+        val defaultJob = GlobalScope.launch {
+            LogUtil.e(TAG, "testCoroutineStart : thread = ${Thread.currentThread().name} ;; default")
+        }
+        defaultJob.cancel()
+        val lazyJob = GlobalScope.launch(start = CoroutineStart.LAZY){
+            LogUtil.e(TAG, "testCoroutineStart : thread = ${Thread.currentThread().name} ;; lazyJob")
+        }
+        val atomicJob = GlobalScope.launch(start = CoroutineStart.ATOMIC) {
+            LogUtil.e(TAG, "testCoroutineStart : thread = ${Thread.currentThread().name} ;; atomic before")
+            delay(100)
+            LogUtil.e(TAG, "testCoroutineStart : thread = ${Thread.currentThread().name} ;; atomic after")
+        }
+        atomicJob.cancel()
+        val undJob = GlobalScope.launch(start = CoroutineStart.UNDISPATCHED) {
+            LogUtil.e(TAG, "testCoroutineStart : thread = ${Thread.currentThread().name} ;; undispatched before")
+            delay(100)
+            LogUtil.e(TAG, "testCoroutineStart : thread = ${Thread.currentThread().name} ;; undispatched after")
+        }
+        undJob.cancel()
+
+        GlobalScope.launch {
+            LogUtil.e(TAG, "context : 父协程 = ${coroutineContext}")
+            launch(CoroutineName("第一个子协程")) {
+                LogUtil.e(TAG, "context :11 = $coroutineContext")
+            }
+            launch(CoroutineName("第二个子协程")) {
+                LogUtil.e(TAG, "context :22 = $coroutineContext")
+            }
+        }
+    }
+
+    private fun testCoroutineContext() {
+        val coroutineContext1 = Job() + CoroutineName("这是一个上下文")
+        LogUtil.e(TAG, "testCoroutineContext : thread = ${Thread.currentThread().name} ;; coroutineContext1 = $coroutineContext1")
+        val coroutineContext2 = coroutineContext1 + Dispatchers.Default + CoroutineName("这是第二个上下文")
+        LogUtil.e(TAG, "testCoroutineContext : thread = ${Thread.currentThread().name} ;; coroutineContext2 = $coroutineContext2")
+        val coroutineContext3 = coroutineContext2 + Dispatchers.Main + CoroutineName("这是第三个上下文")
+        LogUtil.e(TAG, "testCoroutineContext : thread = ${Thread.currentThread().name} ;; coroutineContext3 = $coroutineContext3")
+    }
+
+    private fun start() {
+        runBlocking {
+            LogUtil.e(TAG, "启动一个协程 runBlocking ：thread = ${Thread.currentThread().name}")
+        }
+        LogUtil.e(TAG, "start : 11 = ${Thread.currentThread().name}")
+        val launch = GlobalScope.launch() {
+            LogUtil.e(TAG, "启动一个协程 ： GlobalScope : launch : thread = ${Thread.currentThread().name}")
+        }
+        LogUtil.e(TAG, "start :22 = ${Thread.currentThread().name} ;; ${launch}")
+        val async = GlobalScope.async {
+            LogUtil.e(TAG, "启动一个协程 ：GlobalScope ： async : thread = ${Thread.currentThread().name}")
+        }
+        LogUtil.e(TAG,"start : 33 = ${Thread.currentThread().name} ;; $async")
+
+        GlobalScope.launch(Dispatchers.Main) {
+            val result = withContext(Dispatchers.IO){
+                Thread.sleep(2000)
+                "测试协程异步处理"
+            }
+            getViewBinding().tvTitle.text = result
+        }
+    }
 }
