9ac0e3128064fb1c0a4f2dbff284a9639fcb35b4
chenglong <android1225>
2023-12-13 16:48:48 +0800

    feat_添加寻找峰值的代码。

---
+++ algorithm/src/main/java/com/demo/algorithm/leetcode/medium/FindPeakElement.java
@@ -0,0 +1,40 @@
+package com.demo.algorithm.leetcode.medium;
+
+/**
+ * create on 2023/12/13
+ * @author chenglong
+ * description : 寻找峰值
+ *
+ * 峰值元素是指其值严格大于左右相邻值的元素。
+ * 给你一个整数数组nums，找到峰值元素并返回其索引。数组可能包含多个峰值，在这种情况下，返回任何一个峰值所在位置即可。
+ * 你可以假设 nums[-1] = nums[n] = -∞ 。
+ * 你必须实现时间复杂度为O(logn)的算法来解决此问题。
+ *
+ * 示例 1：
+ * 输入：nums = [1,2,3,1]
+ * 输出：2
+ * 解释：3 是峰值元素，你的函数应该返回其索引2。
+ *
+ * 示例 2：
+ * 输入：nums = [1,2,1,3,5,6,4]
+ * 输出：1 或 5
+ * 解释：你的函数可以返回索引 1，其峰值元素为 2；
+ *      或者返回索引 5， 其峰值元素为 6。
+ *
+ * 提示：
+ * 1 <= nums.length <= 1000
+ * -2^31 <= nums[i] <= 2^31 - 1
+ * 对于所有有效的i都有nums[i] != nums[i+1]
+ */
+public class FindPeakElement {
+
+    public int findPeakElement(int[] nums) {
+        int max = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[max]) {
+                max = i;
+            }
+        }
+        return max;
+    }
+}
